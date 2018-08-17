5fd5ef6eb5660b9e7a04381073ce89327a7af414
LucianoNMoreira <lucianonobremoreira@gmail.com>
2018-08-17 12:26:21 -0300

    Set $JS_FILE_URL based on environment

--- config.php
+++ config.php
@@ -2,10 +2,13 @@
 
     //Config SANDBOX or PRODUCTION environment
     $SANDBOX_ENVIRONMENT = true;
+
     
+    $JS_FILE_URL = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
     $PAGSEGURO_API_URL = 'https://ws.pagseguro.uol.com.br/v2';
     if($SANDBOX_ENVIRONMENT){
         $PAGSEGURO_API_URL = 'https://ws.sandbox.pagseguro.uol.com.br/v2';
+        $JS_FILE_URL = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
     }
 
     $PAGSEGURO_EMAIL = 'YOUR_EMAIL';
